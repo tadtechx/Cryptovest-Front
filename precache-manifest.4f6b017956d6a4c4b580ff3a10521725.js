5b204ad5b64b374ac5f0a5ed5b7e31e0c40fbe94
tadtechx <tadtechcreative@gmail.com>
2022-12-29 12:00:13 +0100

    Updates

---
+++ precache-manifest.4f6b017956d6a4c4b580ff3a10521725.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "91c45bca6108c069fa58",
+    "url": "/cryptovest-frontend/static/css/main.661592bf.chunk.css"
+  },
+  {
+    "revision": "91c45bca6108c069fa58",
+    "url": "/cryptovest-frontend/static/js/main.91c45bca.chunk.js"
+  },
+  {
+    "revision": "0f02e6c33e8e913f5fa5",
+    "url": "/cryptovest-frontend/static/js/1.0f02e6c3.chunk.js"
+  },
+  {
+    "revision": "4f35164ce1a4ceef7486",
+    "url": "/cryptovest-frontend/static/js/runtime~main.4f35164c.js"
+  },
+  {
+    "revision": "a368ed9ebf0904076ad4833685ba004c",
+    "url": "/cryptovest-frontend/static/media/homevid.a368ed9e.mp4"
+  },
+  {
+    "revision": "21e9800ff701a6ee39bc2dd0be3f4395",
+    "url": "/cryptovest-frontend/static/media/alam.21e9800f.jpg"
+  },
+  {
+    "revision": "7d7fab50b53588e6f61c87d75520fe43",
+    "url": "/cryptovest-frontend/static/media/kingery.7d7fab50.jpg"
+  },
+  {
+    "revision": "91335244683c8300b55d2f996435c1cc",
+    "url": "/cryptovest-frontend/static/media/gold.91335244.jpeg"
+  },
+  {
+    "revision": "02942e80b16f2026fb1da00eaeeec333",
+    "url": "/cryptovest-frontend/static/media/realestate.02942e80.jpeg"
+  },
+  {
+    "revision": "57bcb5a232dd4c31db456b3b98a3605e",
+    "url": "/cryptovest-frontend/static/media/loan.57bcb5a2.jpeg"
+  },
+  {
+    "revision": "3e849faa1a344f26891a4172650c0f91",
+    "url": "/cryptovest-frontend/static/media/mcards.3e849faa.jpg"
+  },
+  {
+    "revision": "dafe3487df344e8b6c799a3bcf506520",
+    "url": "/cryptovest-frontend/static/media/test1.dafe3487.jpg"
+  },
+  {
+    "revision": "c85c221bfff2c84311d477f278d7d715",
+    "url": "/cryptovest-frontend/static/media/test2.c85c221b.jpg"
+  },
+  {
+    "revision": "ddea9011adc2a98551682eb58b9e0a75",
+    "url": "/cryptovest-frontend/static/media/test3.ddea9011.jpg"
+  },
+  {
+    "revision": "5fc386b7cfb7884dee70b6fff4b727fe",
+    "url": "/cryptovest-frontend/static/media/test4.5fc386b7.jpg"
+  },
+  {
+    "revision": "14be3598ae25eda00eb92cb40a6d2f9c",
+    "url": "/cryptovest-frontend/static/media/license.14be3598.JPG"
+  },
+  {
+    "revision": "03fec82f5232903cec05fe41c70320db",
+    "url": "/cryptovest-frontend/static/media/logo1.03fec82f.jpg"
+  },
+  {
+    "revision": "d74791cb14dec9e7d6c7e80e61ee95ca",
+    "url": "/cryptovest-frontend/static/media/backg.d74791cb.jpg"
+  },
+  {
+    "revision": "508244c9942843a0186c86858745417a",
+    "url": "/cryptovest-frontend/index.html"
+  }
+];
