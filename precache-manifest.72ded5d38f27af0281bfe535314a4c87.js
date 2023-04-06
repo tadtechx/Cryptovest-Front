49fe6c12a1b5ca070b5b523f1bea6b1d7e9eab2c
eustacedike <eustacedyke@gmail.com>
2023-04-06 15:54:47 +0100

    Updates

---
+++ precache-manifest.72ded5d38f27af0281bfe535314a4c87.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeaa3c40ed5d1efd7752",
+    "url": "/static/css/main.4964f321.chunk.css"
+  },
+  {
+    "revision": "eeaa3c40ed5d1efd7752",
+    "url": "/static/js/main.eeaa3c40.chunk.js"
+  },
+  {
+    "revision": "5c7b6ccabb15fbafcb82",
+    "url": "/static/js/1.5c7b6cca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a368ed9ebf0904076ad4833685ba004c",
+    "url": "/static/media/homevid.a368ed9e.mp4"
+  },
+  {
+    "revision": "21e9800ff701a6ee39bc2dd0be3f4395",
+    "url": "/static/media/alam.21e9800f.jpg"
+  },
+  {
+    "revision": "7d7fab50b53588e6f61c87d75520fe43",
+    "url": "/static/media/kingery.7d7fab50.jpg"
+  },
+  {
+    "revision": "91335244683c8300b55d2f996435c1cc",
+    "url": "/static/media/gold.91335244.jpeg"
+  },
+  {
+    "revision": "02942e80b16f2026fb1da00eaeeec333",
+    "url": "/static/media/realestate.02942e80.jpeg"
+  },
+  {
+    "revision": "57bcb5a232dd4c31db456b3b98a3605e",
+    "url": "/static/media/loan.57bcb5a2.jpeg"
+  },
+  {
+    "revision": "3e849faa1a344f26891a4172650c0f91",
+    "url": "/static/media/mcards.3e849faa.jpg"
+  },
+  {
+    "revision": "dafe3487df344e8b6c799a3bcf506520",
+    "url": "/static/media/test1.dafe3487.jpg"
+  },
+  {
+    "revision": "c85c221bfff2c84311d477f278d7d715",
+    "url": "/static/media/test2.c85c221b.jpg"
+  },
+  {
+    "revision": "ddea9011adc2a98551682eb58b9e0a75",
+    "url": "/static/media/test3.ddea9011.jpg"
+  },
+  {
+    "revision": "5fc386b7cfb7884dee70b6fff4b727fe",
+    "url": "/static/media/test4.5fc386b7.jpg"
+  },
+  {
+    "revision": "14be3598ae25eda00eb92cb40a6d2f9c",
+    "url": "/static/media/license.14be3598.JPG"
+  },
+  {
+    "revision": "03fec82f5232903cec05fe41c70320db",
+    "url": "/static/media/logo1.03fec82f.jpg"
+  },
+  {
+    "revision": "d74791cb14dec9e7d6c7e80e61ee95ca",
+    "url": "/static/media/backg.d74791cb.jpg"
+  },
+  {
+    "revision": "0d5233d28ffa150527ee2e97a4967463",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Cryptovest-Front/precache-manifest.cdbc08e64925634aeb75e1d210881882.js"
+  "/precache-manifest.72ded5d38f27af0281bfe535314a4c87.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Cryptovest-Front/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
